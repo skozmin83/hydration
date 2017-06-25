19e613501a85865ba7d27b9c944f42016e109d0c
sergey <sergey.kozmin@gmail.com>
2017-06-25 19:28:03 -0400

    3 sensors

--- hydration.ino
+++ hydration.ino
@@ -1,16 +1,19 @@
 #include "Planter.cpp"
 
-Planter planter1(8, A0, 10, "P8", 20000, 250, 320);
-Planter planter2(9, A1, 11, "P9", 10000, 250, 320);
+Planter planter1(8, A0, 10, "P8-big", 20000, 250, 320);
+Planter planter2(9, A1, 11, "P9-med", 20000, 250, 320);
+Planter planter3(7, A2, 12, "P7-sml", 20000, 250, 320);
 
 void setup() {
     Serial.begin(9600);
     planter1.setup();
     planter2.setup();
+    planter3.setup();
 }
 
 void loop() {
   // todo use https://www.gammon.com.au/serial for non-blocking read to get plan config/status
     planter1.loop();
     planter2.loop();
+    planter3.loop();
 }
